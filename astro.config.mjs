1ee31266427ef39ec6093d1e3594f88cb24c0a1d
xxcellauto-collab <xxcellauto@gmail.com>
2025-11-18 09:21:21 -0500

    Add sitemap integration and update site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,17 @@ import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
-    vite: {
-        plugins: [tailwindcss()]
-    },
-    integrations: [react()],
-    adapter: netlify({
-        devFeatures: {
-            environmentVariables: true
-        }
-    })
+  site: 'https://xxcelltowing.com',
+  vite: {
+    plugins: [tailwindcss()]
+  },
+  integrations: [react(), sitemap()],
+  adapter: netlify({
+    devFeatures: {
+      environmentVariables: true
+    }
+  })
 });
